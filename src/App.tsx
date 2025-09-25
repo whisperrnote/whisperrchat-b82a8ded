92b90ba11229b08fb0baa79906455aad0350b582
nathfavour <11635483+nathfavour@users.noreply.github.com>
2025-09-25 13:13:30 +0000

    refactor: remove all pages except chat page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from './components/ui/sonner';
-import Landing from './pages/Landing';
-import Auth from './pages/Auth';
 import Chat from './pages/Chat';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
+    <>
+      <Chat />
       <Toaster position="top-right" />
-    </Router>
+    </>
   );
 }
 
